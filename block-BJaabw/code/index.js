03ac4df6fa847d1f655601a1541d9e7844559e9e
Vikram Singh Shahi <vikramsinghshahi@gmail.com>
2021-05-27 15:31:58 +0530

    update

--- block-BJaabw/code/index.js
+++ block-BJaabw/code/index.js
@@ -11,10 +11,15 @@ minToSec(2) ➞ 120
 */
 
 // - Write a Function Decleration for above problem
-function minToSec() {
-  // Your code
+function minToSec(num) {
+  let seconds = num * 60 ;
+
+  return seconds
+  
 }
 // - Execute the function with required parameter
+
+minToSec(50)
 
 /* 2. 
 Create a function named isInRange which validates whether a number n is exclusively within the bounds of lower and upper.
@@ -26,10 +31,17 @@ isInRange(1, 10, 19); // false
 */
 
 // - Write a Function Decleration for above problem
-function isInRange() {
-  // Your code
+function isInRange(a ,b, num) {
+  if(num > a && num > b){
+    return false;
+  } else{
+    return true;
+  }
 }
 // - Execute the function with required parameter
+
+isInRange(1, 20, 9);
+
 
 /* 2. calculateBMI
 
@@ -49,9 +61,23 @@ Obese: BMI is 30 or more
 
 */
 
-function calculateBMI() {
-  // Your code
-}
+function calculateBMI(weight , height) {
+
+  let bmi = weight / (height * height)
+  
+    if(bmi < 18.5){
+        return `Underweight`;
+    } else if ( bmi => 18.5 || bmi <=24.9){
+      return `Normal weight`;
+    } else if  (bmi=>25 || bmi<=29.9 ){
+     return `Overweight`;
+    } else if (bmi >= 30){
+      return `Obese:`
+    }else {
+      return `enter a valid input`
+    }
+  } 
+
 
 /* 3. appropiateDrinks
 
@@ -64,8 +90,19 @@ Create a function that take the age are return the appropiate drink based on the
 
 */
 
-function appropiateDrinks() {
-  // Your code
+function appropiateDrinks(age) {
+
+  if(age <= 14){
+     return `drink fruit juice`;
+  } else if (age <=18){
+    return `drink soda`;
+  } else if ( age <=21){
+    return `drink fruit-flavored beer`;
+  } else if (age > 21){
+    return `drink throat-piercing vodka`;
+  } else{
+    return `enter a valid age`
+  }
 }
 
 /* 4. Add two numers or string
@@ -79,8 +116,16 @@ Twist is when user passes anything other than number, or string value you should
 
 */
 
-function sum() {
-  // Your code
+function sum(a , b) { 
+  if ( typeof a === "number" &&   typeof b === "number"){
+     return a + b;
+
+  } else if ( typeof a === "string" && typeof b === "string"){
+    return `${a} ${b}`;
+  } else {
+    return `Enter valid values`;
+  }
+
 }
 
 // Function Test
